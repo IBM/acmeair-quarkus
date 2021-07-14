2b4befb487785c6c091eb9ab0323b3d4ceac7fb2
Kazurnori OGATA <ogatak@jp.ibm.com>
2021-07-14 21:48:50 +0900

    Initial version, just copied from SpringBoot version

---
+++ src/test/java/com/acmeair/quarkus/NativeMainServiceQuarkusApplicationIT.java
@@ -0,0 +1,9 @@
+package com.acmeair.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMainServiceQuarkusApplicationIT extends MainServiceQuarkusApplicationTest {
+
+    // Execute the same tests but in native mode.
+}
