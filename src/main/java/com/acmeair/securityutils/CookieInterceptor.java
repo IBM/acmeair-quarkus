dfdbca18655647333deec00199c9d2d3c311f0c5
Kazurnori OGATA <ogatak@jp.ibm.com>
2021-09-02 17:55:18 +0900

    Change CookieInterceptor to extend javax.servlet.http.HttpFilter

--- src/main/java/com/acmeair/securityutils/CookieInterceptor.java
+++ src/main/java/com/acmeair/securityutils/CookieInterceptor.java
@@ -15,43 +15,41 @@
 *******************************************************************************/
 package com.acmeair.securityutils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpFilter;
+
 import java.io.IOException;
 
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 
 //import org.springframework.ui.ModelMap;
 //import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.core.MultivaluedMap;
-
 import com.acmeair.web.AuthServiceRest;
 
 //public class CookieInterceptor extends HandlerInterceptorAdapter {
-public class CookieInterceptor implements ContainerResponseFilter {
+@SuppressWarnings("serial")
+public class CookieInterceptor extends HttpFilter {
 	@Override
 //    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 //            ModelAndView modelAndView) throws Exception {
-	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) 
+	        throws IOException, ServletException {
 //		super.postHandle(request, response, handler, modelAndView);
+	  chain.doFilter(request, response);
 		
 //		ModelMap modelMap = modelAndView.getModelMap();
 //		String token = (String) modelMap.getOrDefault("token", "");
 //		String login = (String) modelMap.getOrDefault("login", "");
-        String token = (String) request.getProperty("token");
-        String login = (String) request.getProperty("login");
-        if(token == null)  token = "";
-        if(login == null)  login = "";
+      String token = (String) request.getAttribute("token");
+      String login = (String) request.getAttribute("login");
+      if(token == null)  token = "";
+      if(login == null)  login = "";
 
-        MultivaluedMap<String,Object>headers = response.getHeaders();
-//		response.addHeader("Set-Cookie", AuthServiceRest.JWT_COOKIE_NAME + "=" + token + "; Path=/");
-//      response.addHeader("Set-Cookie", AuthServiceRest.USER_COOKIE_NAME + "=" + login + "; Path=/");
-        headers.add("Set-Cookie", AuthServiceRest.JWT_COOKIE_NAME + "=" + token + "; Path=/");
-        headers.add("Set-Cookie", AuthServiceRest.USER_COOKIE_NAME + "=" + login + "; Path=/");
+      response.addHeader("Set-Cookie", AuthServiceRest.JWT_COOKIE_NAME + "=" + token + "; Path=/");
+      response.addHeader("Set-Cookie", AuthServiceRest.USER_COOKIE_NAME + "=" + login + "; Path=/");
 	}
 }
