c0f895df5ee99d3eaf654e0643f825db6d943b61
Kazurnori OGATA <ogatak@jp.ibm.com>
2021-10-13 12:46:55 +0900

    Use JAX-RS annotations for REST handlers

--- src/main/java/com/acmeair/web/FlightServiceRest.java
+++ src/main/java/com/acmeair/web/FlightServiceRest.java
@@ -16,40 +16,57 @@
 
 package com.acmeair.web;
 
-import java.text.ParseException;
+//import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.RequestHeader;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
 
 import com.acmeair.securityutils.SecurityUtils;
 import com.acmeair.service.FlightService;
 
-@RestController
-@RequestMapping("/")
+//@RestController
+//@RequestMapping("/")
+@Path("/")
 public class FlightServiceRest {
 
 
-  @Autowired
+//  @Autowired
+  @Inject
   private FlightService flightService;
 
-  @Autowired
+//  @Autowired
+  @Inject
   private SecurityUtils secUtils;
 
   /**
    * Get flights.
    */
   
-  @RequestMapping(value = "/queryflights", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-  public String getTripFlights(@RequestParam String fromAirport, @RequestParam String toAirport, 
-      @RequestParam Date fromDate, @RequestParam Date returnDate, @RequestParam Boolean oneWay) throws ParseException {
+//  @RequestMapping(value = "/queryflights", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//  public String getTripFlights(@RequestParam String fromAirport, @RequestParam String toAirport, 
+//      @RequestParam Date fromDate, @RequestParam Date returnDate, @RequestParam Boolean oneWay) throws ParseException {
+  @POST
+  @Path("/queryflights")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  public String getTripFlights(@FormParam("fromAirport") String fromAirport, @FormParam("toAirport") String toAirport, 
+      @FormParam("fromDate") Date fromDate, @FormParam("returnDate") Date returnDate, @FormParam("oneWay") Boolean oneWay) {
 
 
     String options = "";
@@ -86,13 +103,23 @@ public class FlightServiceRest {
   /**
    * Get reward miles for flight segment.
    */
-  @RequestMapping(value = "/getrewardmiles", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = "application/json")
+//  @RequestMapping(value = "/getrewardmiles", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = "application/json")
+//  public RewardMilesResponse getRewardMiles(
+//      @RequestHeader(name = "acmeair-id", required = false) String headerId,
+//      @RequestHeader(name = "acmeair-date", required = false) String headerDate,
+//      @RequestHeader(name = "acmeair-sig-body", required = false) String headerSigBody, 
+//      @RequestHeader(name = "acmeair-signature", required = false) String headerSig,
+//      @RequestParam String flightSegment
+  @POST
+  @Path("/getrewardmiles")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.APPLICATION_JSON)
   public RewardMilesResponse getRewardMiles(
-      @RequestHeader(name = "acmeair-id", required = false) String headerId,
-      @RequestHeader(name = "acmeair-date", required = false) String headerDate,
-      @RequestHeader(name = "acmeair-sig-body", required = false) String headerSigBody, 
-      @RequestHeader(name = "acmeair-signature", required = false) String headerSig,
-      @RequestParam String flightSegment
+      @HeaderParam("acmeair-id") String headerId,
+      @HeaderParam("acmeair-date") String headerDate,
+      @HeaderParam("acmeair-sig-body") String headerSigBody, 
+      @HeaderParam("acmeair-signature") String headerSig,
+      @FormParam("flightSegment") String flightSegment
 
       ) {
     
@@ -109,7 +136,8 @@ public class FlightServiceRest {
     return result;
   }
 
-  @RequestMapping("/")
+//  @RequestMapping("/")
+  @Path("/")
   public String checkStatus() {
     return "OK";        
   }
