be8c9c9105e6933e962cdbebf1252b8a66ab6285
Kazurnori OGATA <ogatak@jp.ibm.com>
2021-10-21 08:35:39 +0900

    Delete unnecessary tests

--- src/test/java/com/acmeair/NativeBookingServiceQuarkusApplicationIT.java
+++
@@ -1,9 +0,0 @@
-package com.acmeair;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeBookingServiceQuarkusApplicationIT extends BookingServiceQuarkusApplicationTest {
-
-    // Execute the same tests but in native mode.
-}
