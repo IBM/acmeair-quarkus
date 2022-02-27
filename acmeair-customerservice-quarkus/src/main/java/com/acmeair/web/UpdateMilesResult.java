99c8055824c62fc5dca421d8a29cced1d401b8a4
Kazurnori OGATA <ogatak@jp.ibm.com>
2022-02-28 07:09:14 +0900

    Use SPDX format for copyright

--- acmeair-customerservice-quarkus/src/main/java/com/acmeair/web/UpdateMilesResult.java
+++ acmeair-customerservice-quarkus/src/main/java/com/acmeair/web/UpdateMilesResult.java
@@ -1,3 +1,9 @@
+/*
+Copyright 2019- IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package com.acmeair.web;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
